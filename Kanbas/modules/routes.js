a6749407bedecd8675136f43260f5ec8cf649ff9
akhila1005 <chiguluri.a@northeastern.edu>
2024-04-18 13:17:39 -0400

    a6 final

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,36 +1,59 @@
 import db from "../Database/index.js";
+import * as dao from "./dao.js";
 function ModuleRoutes(app) {
-    app.put("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        const moduleIndex = db.modules.findIndex(
-          (m) => m._id === mid);
-        db.modules[moduleIndex] = {
-          ...db.modules[moduleIndex],
-          ...req.body
-        };
-        res.sendStatus(204);
-      });    
-    app.delete("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        db.modules = db.modules.filter((m) => m._id !== mid);
-        res.sendStatus(200);
-    });
-    app.post("/api/courses/:cid/modules", (req, res) => {
-        const { cid } = req.params;
-        const newModule = {
-            ...req.body,
-            course: cid,
-            _id: new Date().getTime().toString(),
-        };
-        db.modules.push(newModule);
-        res.send(newModule);
-        });
-    
-    app.get("/api/courses/:cid/modules", (req, res) => {
-        const { cid } = req.params;
-        const modules = db.modules
-        .filter((m) => m.course === cid);
+  app.get("/api/courses/:cid/modules", async (req, res) => {
+    const { cid } = req.params;
+    try {
+        const modules = await dao.findModulesByCourseId(cid);
         res.send(modules);
-    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+  app.post("/api/courses/:cid/modules", async (req, res) => {
+    const { cid } = req.params;
+    const newModule = {
+      ...req.body,
+      course: cid,
+      // _id: new Date().getTime().toString(),
+    };
+    try{
+    const module_new = await dao.createModule(newModule);
+    res.send(module_new);
+    }
+    catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  app.delete("/api/modules/:mid",  async (req, res) => {
+    const { mid } = req.params;
+    try{
+    const status = await dao.deleteModule(mid);
+    res.sendStatus(200);
+    }
+    catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  app.put("/api/modules/:mid", async (req, res) => {
+    const { mid } = req.params;
+    const module = req.body;
+    try{
+    const status = await dao.updateModule(mid, module);
+    res.sendStatus(204);
+    }
+    catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+
 }
 export default ModuleRoutes;
